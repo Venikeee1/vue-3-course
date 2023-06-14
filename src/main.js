7344bc7f39892f861349d8f7bf11d16fa6d5a25b
oleksandr_vienik <alexandrVenik@gmail.com>
2023-06-15 02:02:45 +0300

    create project

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+
+// #app - селектор елемента в index.html
+// Саме в цей елемент буде монтуватись застосунок
+createApp(App).mount('#app');
